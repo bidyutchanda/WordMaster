7efb54d1e3263640a04a83741e1ba02497c08c92
guiemi <guiemi.teixeira@gmail.com>
2018-12-30 00:56:27 -0200

    Fixes some details about pythonic syntax

--- wordmaster.py
+++ wordmaster.py
@@ -191,13 +191,15 @@ print("I can tell you everything you want to know about the word that is bugging
 print(f"So, welcome to my v1.0 on this fine day in {datetime.now().strftime('%B.')}")
 word=input("\nInput your word: ")
 
-print("\nYou can do the following things with your word.\n1.Know the meaning.\n2.Translate it to another language.\nThe etymological tree will be displayed in any case. ")
-print("")
-choice=int(input("\nEnter your choice as a number:"))
+# print("\nYou can do the following things with your word.\n.\n2.Translate it to another language.\nThe etymological tree will be displayed in any case. ")
+print("1. Know the meaning")
+print("2. Translate it to another language\n")
+print("(The etymological tree will be displayed in any case)")
+choice=int(input("\nEnter your choice as a number: "))
 
 
-def langCode_to_lang(list,langName):
-	return [langCode for langCode,toLang in list.items() if toLang == langName]
+def langCode_to_lang(list, langName):
+	return [langCode for langCode, toLang in list.items() if toLang == langName]
 	
 
 toTB="" 
@@ -205,26 +207,26 @@ toTB=TextBlob(word)
 toTB_lang=toTB.detect_language()
 print(f"\nThe language of the entered word is: {langcodes[toTB_lang]}")
 
-if choice==1: 
-	if toTB_lang!='en':
-		syns = wordnet.synsets(toTB.translate(to='en')) 
+if choice == 1: 
+	if toTB_lang != 'en':
+		syns = wordnet.synsets(toTB.translate(to = 'en')) 
 	else:
 		syns = wordnet.synsets(word)
 	print(f"The most common meaning of '{word}' in English is:")
 	print(syns[0].definition()) 
 
-elif choice==2: 
-	toLang=input('Which language do you want the word to be translated in? ')
-	langCode=langCode_to_lang(langcodes,toLang) 
+elif choice == 2: 
+	toLang = input('Which language do you want the word to be translated in? ')
+	langCode = langCode_to_lang(langcodes, toLang) 
 	print(f"'{word}' translated into {toLang} is:")
-	print(toTB.translate(to=langCode[0])) 
+	print(toTB.translate(to = langCode[0])) 
 
 
 
-toTB2=TextBlob(word) 
-toTB2_lang=toTB.detect_language()
+toTB2 = TextBlob(word) 
+toTB2_lang = toTB.detect_language()
 print(f"The etymological root of '{word}' is as below: \n")
-if toTB2_lang!='en':
-	print(ety.tree(str(toTB2.translate(to='en'))))
+if toTB2_lang != 'en':
+	print(ety.tree(str(toTB2.translate(to = 'en'))))
 else:
 	print (ety.tree(word))
